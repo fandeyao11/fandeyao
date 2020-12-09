b56a12fc915376997e8ca7de79efe71264fc6d5c
fandeyao11 <74449257+fandeyao11@users.noreply.github.com>
2020-12-09 16:37:38 +0800

    Initial commit

---
+++ js/c3.js
@@ -0,0 +1,37 @@
+window.addEventListener('load', function() {
+    $(".icon1").click(function(){
+        $(".icon1").attr("src","../upload/icon-area3.png");
+        $(".icon-html").html("已收藏");
+    });
+    $(".icon-html").click(function(){
+        $(".icon1").attr("src","../upload/icon_area2.png");
+        $(".icon-html").html("收藏");
+    });
+ 
+   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
